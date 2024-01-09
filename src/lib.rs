894350380e3a6120620bc06ffe66fa8a168e5beb
Thomas Marchand <thomas.marchand@tuta.io>
2024-01-09 15:16:52 +0000

    fix: support multiple routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,29 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, ItemFn};
+use quote::{quote, format_ident};
+use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Lit};
 
 #[proc_macro_attribute]
 pub fn route(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as AttributeArgs);
     let function = parse_macro_input!(input as ItemFn);
     let function_name = &function.sig.ident;
-    let route_path = match &args[0] {
-        syn::NestedMeta::Lit(syn::Lit::Str(lit_str)) => lit_str,
+
+    let route_path = match args.first().expect("Expected a route path argument") {
+        NestedMeta::Lit(Lit::Str(lit_str)) => lit_str,
         _ => panic!("Expected a string literal for the route path"),
     };
-    let handler_path = quote! { #function_name };
+
+    // Generate a unique identifier for the register function
+    let register_function_name = format_ident!("register_{}", function_name);
+
     let expanded = quote! {
         #function
 
         #[ctor::ctor]
-        fn register_route() {
+        fn #register_function_name() {
             use axum::{routing::get, Router};
-            let route = Router::new().route(#route_path, get(#handler_path));
+            let route = Router::new().route(#route_path, get(crate::#function_name));
             crate::ROUTE_REGISTRY.lock().unwrap().push(route);
         }
     };
