17e60c635465b5f2ccef16856a556923078b1c0b
Thomas Marchand <thomas.marchand@tuta.io>
2024-01-09 15:12:15 +0000

    fix: path

--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,15 @@ pub fn route(args: TokenStream, input: TokenStream) -> TokenStream {
         syn::NestedMeta::Lit(syn::Lit::Str(lit_str)) => lit_str,
         _ => panic!("Expected a string literal for the route path"),
     };
-
+    let handler_path = quote! { #function_name };
     let expanded = quote! {
         #function
 
         #[ctor::ctor]
         fn register_route() {
             use axum::{routing::get, Router};
-            let route = Router::new().route(#route_path, get(super::#function_name));
-            super::ROUTE_REGISTRY.lock().unwrap().push(route);
+            let route = Router::new().route(#route_path, get(#handler_path));
+            crate::ROUTE_REGISTRY.lock().unwrap().push(route);
         }
     };
 
